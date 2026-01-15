832adbc028af7815b2fd8d401d8a56c292143f1d
NDK <9401ndk@naver.com>
2026-01-15 21:41:08 +0900

    왕실의 나이트 업데이트

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import 주요알고리즘이론과실전문제_02.상하좌우;
 import 주요알고리즘이론과실전문제_02.시각;
+import 주요알고리즘이론과실전문제_02.왕실의나이트_002;
 
 public class Main {
     public static void main(String[] args) {
-        시각 obj = new 시각();
+        왕실의나이트_002 obj = new 왕실의나이트_002();
 
         int rs = obj.solve();
         System.out.println(rs);
--- src/주요알고리즘이론과실전문제_02/왕실의나이트_002.java
+++ src/주요알고리즘이론과실전문제_02/왕실의나이트_002.java
@@ -2,6 +2,8 @@ package 주요알고리즘이론과실전문제_02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 왕실의나이트_002 {
@@ -12,18 +14,33 @@ public class 왕실의나이트_002 {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            String steps = st.nextToken();
+            String step = st.nextToken();
 
-            char[] arr = steps.toCharArray();
+            char[] arr = step.toCharArray();
 
-            char h = arr[0];
-            char v = arr[1];
+            int x = arr[0] - 'a' + 1; // 1 ~ 8
+            int y = arr[1] - '0';     // 1 ~ 8
 
-            if(h){
 
+            // 검색의 경우에는 검색 방향을 List에 담아두고 반복문으로 검색한다.
+            List<int[]> steps = new ArrayList<>();
+            steps.add(new int[]{-2,-1});
+            steps.add(new int[]{-1,-2});
+            steps.add(new int[]{1, -2});
+            steps.add(new int[]{2, -1});
+            steps.add(new int[]{2, 1});
+            steps.add(new int[]{1, 2});
+            steps.add(new int[]{-1, 2});
+            steps.add(new int[]{-2, 1});
+
+            for(int[] s : steps){
+                int next_x = x + s[0];
+                int next_y = y + s[1];
+
+                if (next_x >= 1 && next_x <= 8 && next_y >= 1 && next_y <= 8) {
+                    result++;
+                }
             }
-
-            int N = Integer.parseInt(st.nextToken());
 
             return result;
         }catch (Exception e){
